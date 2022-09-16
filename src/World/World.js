22ee4db2629c72fa47b0b380368fe72d4ac04832
Aleksandar Hristov <aleksandar.vladimirov@falmouth.ac.uk>
2022-09-16 11:05:43 +0100

    123123

--- src/World/World.js
+++ src/World/World.js
@@ -1,3 +1,4 @@
+import { loadBirds } from './components/birds/birds.js';
 import { createCamera } from './components/camera.js';
 import { createLights } from './components/lights.js';
 import { createScene } from './components/scene.js';
@@ -8,6 +9,7 @@ import { Resizer } from './systems/Resizer.js';
 import { Loop } from './systems/Loop.js';
 
 let camera;
+let controls;
 let renderer;
 let scene;
 let loop;
@@ -19,14 +21,23 @@ class World {
     scene = createScene();
     loop = new Loop(camera, scene, renderer);
     container.append(renderer.domElement);
+    controls = createControls(camera, renderer.domElement);
 
-    const controls = createControls(camera, renderer.domElement);
     const { ambientLight, mainLight } = createLights();
 
     loop.updatables.push(controls);
     scene.add(ambientLight, mainLight);
 
     const resizer = new Resizer(container, camera, renderer);
+  }
+
+  async init() {
+    const { parrot, flamingo, stork } = await loadBirds();
+
+    // move the target to the center of the front bird
+    controls.target.copy(parrot.position);
+
+    scene.add(parrot, flamingo, stork);
   }
 
   render() {
